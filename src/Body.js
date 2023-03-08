db3c2f0eb2893b2ee0814f0a423f65baec10f90f
RicardoOrtega667 <ricardo_or93@outlook.com>
2023-03-07 21:25:21 -0600

    first commit

---
+++ src/Body.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export const Body = () => {
+  return (
+    <body>
+      <section></section>
+      <aside></aside>
+    </body>
+  );
+};
+
+export default Body;
---
+++ src/NavBar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export const NavBar = () => {
+  return (
+    <header>
+      <nav> 
+        <span>Professional Developer Portfolio</span>
+        <p>Hi! My name is Ricardo Ivan Ortega Ruiz <br/> and I am Full Stack Developer!</p>
+
+
+
+      </nav>
+    </header>
+    
+)}
+
+export default NavBar;
